9e34bd2eeab0c14c758e276e780279f1eff54d47
bansaltushar014 <bansaltushar014@gmail.com>
2019-12-25 22:35:06 +0530

    commit

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
 var METoken = artifacts.require("METoken");
 
 module.exports = function(deployer) {
-  // Deploy the METoken contract as our only task
+  // Deploy the METoken .contract as our only task
   deployer.deploy(METoken);
 };
